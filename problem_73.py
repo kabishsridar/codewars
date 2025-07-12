d3614badb73994e54e0997de467affa4791a920d
Kabish <kabishsridar6@gmail.com>
2025-07-12 10:28:21 +0530

    solved today's codewars problems

---
+++ problem_73.py
@@ -0,0 +1,21 @@
+"""
+Given a list of integers values, your job is to return the sum of the values; however, if the same integer value appears multiple times in the list, you can only count it once in your sum.
+
+For example:
+
+[ 1, 2, 3] ==> 6
+
+[ 1, 3, 8, 1, 8] ==> 12
+
+[ -1, -1, 5, 2, -7] ==> -1
+
+[] ==> None
+"""
+def unique_sum(lst):
+    numbers = set()
+    for num in lst:
+        numbers.add(num)
+    if numbers:
+        return sum(numbers)
+    else:
+        return None
