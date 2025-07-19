467466d4ba6639a99e41eb83e24808d587a85fb2
Kabish <kabishsridar6@gmail.com>
2025-07-19 10:12:35 +0530

    solved codewars problems

---
+++ problem_87.py
@@ -0,0 +1,16 @@
+""" In this kata, your job is to return the two distinct highest values in a list. If there're less than 2 unique values, return as many of them, as possible.
+
+The result should also be ordered from highest to lowest.
+
+Examples:
+
+[4, 10, 10, 9]  =>  [10, 9]
+[1, 1, 1]  =>  [1]
+[]  =>  [] """
+
+def two_highest(arg1):
+    unique = list(set(arg1))
+    if not unique:
+        return []
+    unique.sort(reverse=True)
+    return unique[:2]
